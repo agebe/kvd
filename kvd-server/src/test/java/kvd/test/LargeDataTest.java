85dbfc98792161ab366520e9594d857c8cf8c8f0
Andre Gebers <andre.gebers@gmail.com>
2021-01-15 06:45:24 +1000

    write/read-verfiy 1,2,4,8gb key/values

---
+++ kvd-server/src/test/java/kvd/test/LargeDataTest.java
@@ -0,0 +1,95 @@
+package kvd.test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+import kvd.client.KvdClient;
+import kvd.common.KvdException;
+
+public class LargeDataTest {
+
+  private static String key(int mb) {
+    return mb + "mb";
+  }
+
+  private static void insertLarge(int mb, KvdClient client) {
+    String key = key(mb);
+    System.out.println("writing key " + key);
+    try(OutputStream out = client.put(key)) {
+      Random r = new Random(mb);
+      byte[] buf = new byte[1024 * 64];
+      IntStream.range(0, mb).forEachOrdered(i -> {
+        IntStream.range(0, 16).forEachOrdered(block -> {
+          r.nextBytes(buf);
+          try {
+            out.write(buf);
+          } catch(IOException e) {
+            throw new KvdException("write failed", e);
+          }
+        });
+      });
+    } catch(IOException e) {
+      throw new KvdException("insert failed", e);
+    }
+  }
+
+  private static int readFully(InputStream in, byte[] buf) throws IOException {
+    int total = 0;
+    while(true) {
+      int read = in.read(buf, total, buf.length-total);
+      if(read == -1) {
+        if(total == 0) {
+          return -1;
+        } else {
+          return total;
+        }
+      } else {
+        total += read;
+        if(total == buf.length) {
+          return total;
+        }
+      }
+    }
+  }
+
+  private static void read(int mb, KvdClient client) {
+    String key = key(mb);
+    System.out.println("reading key " + key);
+    try(InputStream in = client.get(key)) {
+      Random r = new Random(mb);
+      byte[] expectedBuf = new byte[1024 * 64];
+      byte[] buf = new byte[1024 * 64];
+      while(true) {
+        r.nextBytes(expectedBuf);
+        int read = readFully(in, buf);
+        if(read == -1) {
+          break;
+        }
+        if(!Arrays.equals(expectedBuf, 0, read, buf, 0, read)) {
+          throw new KvdException("read verification failed");
+        }
+      }
+    } catch(IOException e) {
+      throw new KvdException("get failed on " + key, e);
+    }
+  }
+
+  public static void main(String[] args) {
+    try(KvdClient client = new KvdClient("localhost")) {
+      IntStream.rangeClosed(0, 3).forEachOrdered(i -> client.remove(key((1<<i)*1024)));
+      System.out.println("remove done");
+      IntStream.rangeClosed(0, 3).forEachOrdered(i -> insertLarge((1<<i)*1024, client));
+      System.out.println("write done");
+      IntStream.rangeClosed(0, 3).forEachOrdered(i -> read((1<<i)*1024, client));
+      System.out.println("read done");
+    } catch(Exception e) {
+      e.printStackTrace();
+    }
+    System.out.println("done");
+  }
+
+}
