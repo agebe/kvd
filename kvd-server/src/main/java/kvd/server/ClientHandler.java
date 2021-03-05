1c8bc592452ebf85692f82d95596cb0b9d45c2bf
Andre Gebers <andre.gebers@gmail.com>
2021-03-06 03:00:58 +1000

    refactor, code cleanup

--- kvd-server/src/main/java/kvd/server/ClientHandler.java
+++ kvd-server/src/main/java/kvd/server/ClientHandler.java
@@ -19,9 +19,12 @@ import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Consumer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.ImmutableMap;
 
 import kvd.common.KvdException;
 import kvd.common.Utils;
@@ -52,6 +55,23 @@ public class ClientHandler implements Runnable, AutoCloseable {
   private ClientResponseHandler client;
 
   private Thread clientThread;
+
+  // how to react to client packets
+  private Map<PacketType, Consumer<Packet>> packetConsumers = ImmutableMap.<PacketType, Consumer<Packet>>builder()
+      .put(PacketType.PING, this::ping)
+      .put(PacketType.BYE, this::bye)
+      .put(PacketType.PUT_INIT, this::putInit)
+      .put(PacketType.PUT_DATA, this::put)
+      .put(PacketType.PUT_FINISH, this::put)
+      .put(PacketType.PUT_ABORT, this::put)
+      .put(PacketType.GET_INIT, this::getInit)
+      .put(PacketType.CLOSE_CHANNEL, this::closeChannel)
+      .put(PacketType.CONTAINS_REQUEST, this::containsRequest)
+      .put(PacketType.REMOVE_REQUEST, this::removeRequest)
+      .put(PacketType.TX_BEGIN, this::txBegin)
+      .put(PacketType.TX_COMMIT, this::txCommit)
+      .put(PacketType.TX_ROLLBACK, this::txRollback)
+      .build();
 
   public ClientHandler(long clientId, Socket socket, StorageBackend storage) {
     this.clientId = clientId;
@@ -107,144 +127,172 @@ public class ClientHandler implements Runnable, AutoCloseable {
     }
   }
 
-  @SuppressWarnings("resource")
-  private void handlePacket(Packet packet) {
-    if(PacketType.PING.equals(packet.getType())) {
-      client.sendAsync(Packets.packet(PacketType.PONG));
-    } else if(PacketType.BYE.equals(packet.getType())) {
-      log.debug("client '{}' close received", clientId);
-      closeAllChannels();
-      rollbackAllTransactions();
-      client.sendAsync(Packets.packet(PacketType.BYE));
-      closed.set(true);
-    } else if(PacketType.PUT_INIT.equals(packet.getType())) {
+  private void ping(Packet packet) {
+    client.sendAsync(Packets.packet(PacketType.PONG));
+  }
+
+  private void bye(Packet packet) {
+    log.debug("client '{}' close received", clientId);
+    closeAllChannels();
+    rollbackAllTransactions();
+    client.sendAsync(Packets.packet(PacketType.BYE));
+    closed.set(true);
+  }
+
+  private void putInit(Packet packet) {
+    int txId = packet.getTx();
+    Transaction tx = transactions.get(txId);
+    log.debug("put init, txId '{}', tx '{}'", txId, tx);
+    if((txId!=0) && (tx==null)) {
+      log.warn("received put init for tx '{}' but transaction does not exit", txId);
+      client.sendAsync(Packets.packet(PacketType.PUT_ABORT, packet.getChannel()));
+    } else {
+      createChannel(packet, new PutConsumer(storage, client, tx));
+    }
+  }
+
+  private void put(Packet packet) {
+    int channel = packet.getChannel();
+    ChannelConsumer c = channels.get(channel);
+    if(c != null) {
+      c.accept(packet);
+    } else {
+      throw new KvdException("channel does not exist " + channel);
+    }
+  }
+
+  private void getInit(Packet packet) {
+    int txId = packet.getTx();
+    Transaction tx = transactions.get(txId);
+    log.debug("get req, txId '{}', tx '{}'", txId, tx);
+    if((txId!=0) && (tx==null)) {
+      log.warn("received get init for tx '{}' but transaction does not exit", txId);
+      client.sendAsync(Packets.packet(PacketType.GET_ABORT, packet.getChannel()));
+    } else {
+      createChannel(packet, new GetConsumer(clientId, packet.getChannel(), storage, client, tx));
+    }
+  }
+
+  private void closeChannel(Packet packet) {
+    ChannelConsumer c = channels.remove(packet.getChannel());
+    if(c != null) {
+      try {
+        c.close();
+      } catch(Exception e) {
+        log.debug("failed to close channel", e);
+      }
+    }
+  }
+
+  private void containsRequest(Packet packet) {
+    String key = packet.getStringBody().getStr();
+    if(Keys.isInternalKey(key)) {
+      client.sendAsync(Packets.packet(PacketType.CONTAINS_ABORT, packet.getChannel()));
+    } else {
       int txId = packet.getTx();
       Transaction tx = transactions.get(txId);
-      log.debug("put init, txId '{}', tx '{}'", txId, tx);
+      log.debug("contains req, txId '{}', tx '{}'", txId, tx);
       if((txId!=0) && (tx==null)) {
-        log.warn("received put init for tx '{}' but transaction does not exit", txId);
-        client.sendAsync(Packets.packet(PacketType.PUT_ABORT, packet.getChannel()));
-      } else {
-        createChannel(packet, new PutConsumer(storage, client, tx));
-      }
-    } else if(
-        PacketType.PUT_DATA.equals(packet.getType()) ||
-        PacketType.PUT_FINISH.equals(packet.getType()) ||
-        PacketType.PUT_ABORT.equals(packet.getType())) {
-      int channel = packet.getChannel();
-      ChannelConsumer c = channels.get(channel);
-      if(c != null) {
-        c.accept(packet);
-      } else {
-        throw new KvdException("channel does not exist " + channel);
-      }
-    } else if(PacketType.GET_INIT.equals(packet.getType())) {
-      int txId = packet.getTx();
-      Transaction tx = transactions.get(txId);
-      log.debug("get req, txId '{}', tx '{}'", txId, tx);
-      if((txId!=0) && (tx==null)) {
-        log.warn("received get init for tx '{}' but transaction does not exit", txId);
-        client.sendAsync(Packets.packet(PacketType.GET_ABORT, packet.getChannel()));
-      } else {
-        createChannel(packet, new GetConsumer(clientId, packet.getChannel(), storage, client, tx));
-      }
-    } else if(PacketType.CLOSE_CHANNEL.equals(packet.getType())) {
-      closeChannel(packet.getChannel());
-    } else if(PacketType.CONTAINS_REQUEST.equals(packet.getType())) {
-      String key = packet.getStringBody().getStr();
-      if(Keys.isInternalKey(key)) {
+        log.warn("received contains request for tx '{}' but transaction does not exit", txId);
         client.sendAsync(Packets.packet(PacketType.CONTAINS_ABORT, packet.getChannel()));
-      } else {
-        int txId = packet.getTx();
-        Transaction tx = transactions.get(txId);
-        log.debug("contains req, txId '{}', tx '{}'", txId, tx);
-        if((txId!=0) && (tx==null)) {
-          log.warn("received contains request for tx '{}' but transaction does not exit", txId);
-          client.sendAsync(Packets.packet(PacketType.CONTAINS_ABORT, packet.getChannel()));
-        } else if(tx == null) {
-          // TODO change CONTAINS_RESPONSE body to "true" or "false" (StringBody)
-          storage.withTransactionVoid(newTx -> {
-            boolean contains = storage.contains(newTx, key);
-            client.sendAsync(Packets.packet(PacketType.CONTAINS_RESPONSE,
-                packet.getChannel(), new byte[] {(contains?(byte)1:(byte)0)}));
-          });
-        } else {
-          boolean contains = storage.contains(tx, key);
+      } else if(tx == null) {
+        // TODO change CONTAINS_RESPONSE body to "true" or "false" (StringBody)
+        storage.withTransactionVoid(newTx -> {
+          boolean contains = storage.contains(newTx, key);
           client.sendAsync(Packets.packet(PacketType.CONTAINS_RESPONSE,
               packet.getChannel(), new byte[] {(contains?(byte)1:(byte)0)}));
-        }
-      }
-    } else if(PacketType.REMOVE_REQUEST.equals(packet.getType())) {
-      String key = packet.getStringBody().getStr();
-      if(Keys.isInternalKey(key)) {
-        client.sendAsync(Packets.packet(PacketType.REMOVE_ABORT, packet.getChannel()));
+        });
       } else {
-        int txId = packet.getTx();
-        Transaction tx = transactions.get(txId);
-        log.debug("contains req, txId '{}', tx '{}'", txId, tx);
-        if((txId!=0) && (tx==null)) {
-          log.warn("received remove request for tx '{}' but transaction does not exit", txId);
-          client.sendAsync(Packets.packet(PacketType.REMOVE_ABORT, packet.getChannel()));
-        } else if(tx == null) {
-          storage.withTransactionVoid(Newtx -> {
-            boolean removed = storage.remove(Newtx, key);
-            client.sendAsync(Packets.packet(PacketType.REMOVE_RESPONSE,
-                packet.getChannel(), new byte[] {(removed?(byte)1:(byte)0)}));
-          });
-        } else {
-          boolean removed = storage.remove(tx, key);
+        boolean contains = storage.contains(tx, key);
+        client.sendAsync(Packets.packet(PacketType.CONTAINS_RESPONSE,
+            packet.getChannel(), new byte[] {(contains?(byte)1:(byte)0)}));
+      }
+    }
+  }
+
+  private void removeRequest(Packet packet) {
+    String key = packet.getStringBody().getStr();
+    if(Keys.isInternalKey(key)) {
+      client.sendAsync(Packets.packet(PacketType.REMOVE_ABORT, packet.getChannel()));
+    } else {
+      int txId = packet.getTx();
+      Transaction tx = transactions.get(txId);
+      log.debug("contains req, txId '{}', tx '{}'", txId, tx);
+      if((txId!=0) && (tx==null)) {
+        log.warn("received remove request for tx '{}' but transaction does not exit", txId);
+        client.sendAsync(Packets.packet(PacketType.REMOVE_ABORT, packet.getChannel()));
+      } else if(tx == null) {
+        storage.withTransactionVoid(Newtx -> {
+          boolean removed = storage.remove(Newtx, key);
           client.sendAsync(Packets.packet(PacketType.REMOVE_RESPONSE,
               packet.getChannel(), new byte[] {(removed?(byte)1:(byte)0)}));
-        }
-      }
-    } else if(PacketType.TX_BEGIN.equals(packet.getType())) {
-      Transaction tx = storage.begin();
-      int txId = tx.handle();
-      if(txId >= 1) {
-        transactions.put(txId, tx);
-        client.sendAsync(Packets.packet(PacketType.TX_BEGIN, packet.getChannel(), txId));
+        });
       } else {
-        log.error("wrong txId '{}', must be >= 1", txId);
-        client.sendAsync(Packets.packet(PacketType.TX_ABORT, packet.getChannel(), txId));
+        boolean removed = storage.remove(tx, key);
+        client.sendAsync(Packets.packet(PacketType.REMOVE_RESPONSE,
+            packet.getChannel(), new byte[] {(removed?(byte)1:(byte)0)}));
       }
-    } else if(PacketType.TX_COMMIT.equals(packet.getType())) {
-      int txId = packet.getTx();
-      Transaction tx = transactions.get(txId);
-      log.debug("tx commit, txId '{}', tx '{}'", txId, tx);
-      if((txId!=0) && (tx==null)) {
-        log.warn("received tx commit for txId '{}' but transaction does not exit", txId);
-      } else if(txId == 0) {
-        log.warn("received tx commit for txId 0 (NO_TX), ignore");
-      } else {
+    }
+  }
+
+  private void txBegin(Packet packet) {
+    Transaction tx = storage.begin();
+    int txId = tx.handle();
+    if(txId >= 1) {
+      transactions.put(txId, tx);
+      client.sendAsync(Packets.packet(PacketType.TX_BEGIN, packet.getChannel(), txId));
+    } else {
+      log.error("wrong txId '{}', must be >= 1", txId);
+      client.sendAsync(Packets.packet(PacketType.TX_ABORT, packet.getChannel(), txId));
+    }
+  }
+
+  private void txCommit(Packet packet) {
+    int txId = packet.getTx();
+    Transaction tx = transactions.get(txId);
+    log.debug("tx commit, txId '{}', tx '{}'", txId, tx);
+    if((txId!=0) && (tx==null)) {
+      log.warn("received tx commit for txId '{}' but transaction does not exit", txId);
+    } else if(txId == 0) {
+      log.warn("received tx commit for txId 0 (NO_TX), ignore");
+    } else {
+      try {
+        tx.commit();
+      } finally {
         try {
-          tx.commit();
+          client.sendAsync(Packets.packet(PacketType.TX_CLOSED, packet.getChannel(), txId));
         } finally {
-          try {
-            client.sendAsync(Packets.packet(PacketType.TX_CLOSED, packet.getChannel(), txId));
-          } finally {
-            transactions.remove(txId);
-          }
+          transactions.remove(txId);
         }
       }
-    } else if(PacketType.TX_ROLLBACK.equals(packet.getType())) {
-      int txId = packet.getTx();
-      Transaction tx = transactions.get(txId);
-      log.debug("tx rollback, txId '{}', tx '{}'", txId, tx);
-      if((txId!=0) && (tx==null)) {
-        log.warn("received tx rollback for txId '{}' but transaction does not exit", txId);
-      } else if(txId == 0) {
-        log.warn("received tx rollback for txId 0 (NO_TX), ignore");
-      } else {
+    }
+  }
+
+  private void txRollback(Packet packet) {
+    int txId = packet.getTx();
+    Transaction tx = transactions.get(txId);
+    log.debug("tx rollback, txId '{}', tx '{}'", txId, tx);
+    if((txId!=0) && (tx==null)) {
+      log.warn("received tx rollback for txId '{}' but transaction does not exit", txId);
+    } else if(txId == 0) {
+      log.warn("received tx rollback for txId 0 (NO_TX), ignore");
+    } else {
+      try {
+        tx.rollback();
+      } finally {
         try {
-          tx.rollback();
+          client.sendAsync(Packets.packet(PacketType.TX_CLOSED, packet.getChannel(), txId));
         } finally {
-          try {
-            client.sendAsync(Packets.packet(PacketType.TX_CLOSED, packet.getChannel(), txId));
-          } finally {
-            transactions.remove(txId);
-          }
+          transactions.remove(txId);
         }
       }
+    }
+  }
+
+  private void handlePacket(Packet packet) {
+    Consumer<Packet> c = packetConsumers.get(packet.getType());
+    if(c != null) {
+      c.accept(packet);
     } else {
       log.error("can't handle packet type '{}' (not implemented)", packet.getType());
       throw new KvdException("server error, can't handle packet type " + packet.getType());
@@ -259,17 +307,6 @@ public class ClientHandler implements Runnable, AutoCloseable {
       c.accept(packet);
     } else {
       throw new KvdException("client error, channel already exists " + channel);
-    }
-  }
-
-  private void closeChannel(int channel) {
-    ChannelConsumer c = channels.remove(channel);
-    if(c != null) {
-      try {
-        c.close();
-      } catch(Exception e) {
-        log.debug("failed to close channel", e);
-      }
     }
   }
 
