fa5180e34e9c5711d62fd2c16fce2004f6be2416
Andre Gebers <andre.gebers@gmail.com>
2021-03-27 06:09:20 +1000

    improved junit tests

--- kvd-server/src/test/java/kvd/test/ConcurrencyControlPesWTest.java
+++ kvd-server/src/test/java/kvd/test/ConcurrencyControlPesWTest.java
@@ -31,7 +31,7 @@ import kvd.common.KvdException;
 import kvd.server.ConcurrencyControl;
 import kvd.server.Kvd;
 
-public class ConcurrencyControlPesRWTest {
+public class ConcurrencyControlPesWTest {
 
   protected static Kvd server;
 
@@ -41,7 +41,7 @@ public class ConcurrencyControlPesRWTest {
 
   @BeforeAll
   public static void setup() {
-    server = TestUtils.startMemServer("warn", ConcurrencyControl.PESRW);
+    server = TestUtils.startMemServer("warn", ConcurrencyControl.PESW);
   }
 
   @AfterAll
@@ -51,7 +51,7 @@ public class ConcurrencyControlPesRWTest {
 
   @Test
   public void concurrentRead1() {
-    final String key = "concurrentRead1_pesrw";
+    final String key = "concurrentRead1_pesw";
     final String value = key;
     try(KvdClient client = client()) {
       assertFalse(client.contains(key));
@@ -70,7 +70,7 @@ public class ConcurrencyControlPesRWTest {
 
   @Test
   public void concurrentReadWrite1() throws Exception {
-    final String key = "concurrentReadWrite1_pesrw";
+    final String key = "concurrentReadWrite1_pesw";
     final String value1 = key;
     final String value2 = value1+"value2";
     try(KvdClient client = client()) {
@@ -82,54 +82,12 @@ public class ConcurrencyControlPesRWTest {
       tx1.putString(key, value2);
       assertEquals(value2, tx1.getString(key));
       assertTrue(tx1.contains(key));
-      assertThrows(KvdException.class, () -> tx2.getString(key));
-      assertThrows(KvdException.class, () -> tx2.contains(key));
+      assertEquals(value1, tx2.getString(key));
+      assertTrue(tx2.contains(key));
       tx1.commit();
+      assertEquals(value2, tx2.getString(key));
+      tx2.rollback();
       assertTrue(client.remove(key));
-    }
-  }
-
-  @Test
-  public void concurrentReadWrite2() throws Exception {
-    // testing get write lock in t2
-    CompletableFuture<Boolean> ft1 = new CompletableFuture<>();
-    CompletableFuture<Boolean> ft2 = new CompletableFuture<>();
-    final String key = "concurrentReadWrite2_pesrw";
-    CompletableFuture<Boolean> f1 = new CompletableFuture<>();
-    try(KvdClient client = client()) {
-      assertFalse(client.contains(key));
-      client.putString(key, "1");
-      Thread t1 = new Thread(() -> {
-        try(KvdTransaction tx = client.beginTransaction()) {
-          assertTrue(tx.contains(key));
-          f1.complete(true);
-          Thread.sleep(100);
-          tx.putString(key, "2");
-          Thread.sleep(100);
-          tx.commit();
-          ft1.complete(true);
-        } catch(Throwable t) {
-          ft1.completeExceptionally(new RuntimeException("t1 failed", t));
-        }
-      });
-      Thread t2 = new Thread(() -> {
-        try(KvdTransaction tx = client.beginTransaction()) {
-          f1.get();
-          tx.putString(key, "3");
-          assertEquals("3", tx.getString(key));
-          tx.commit();
-          ft2.complete(true);
-        } catch(Throwable t) {
-          ft2.completeExceptionally(new RuntimeException("t2 failed", t));
-        }
-      });
-      t1.start();
-      t2.start();
-      t1.join();
-      t2.join();
-      ft1.get();
-      ft2.get();
-      assertEquals("3", client.getString(key));
     }
   }
 
@@ -138,7 +96,7 @@ public class ConcurrencyControlPesRWTest {
     // testing write lock upgrade in t1
     CompletableFuture<Boolean> ft1 = new CompletableFuture<>();
     CompletableFuture<Boolean> ft2 = new CompletableFuture<>();
-    final String key = "concurrentReadWrite3_pesrw";
+    final String key = "concurrentReadWrite3_pesw";
     CompletableFuture<Boolean> f1 = new CompletableFuture<>();
     try(KvdClient client = client()) {
       assertFalse(client.contains(key));
@@ -177,26 +135,27 @@ public class ConcurrencyControlPesRWTest {
 
   @Test
   public void testDeadlock1() throws Exception {
-    // neither thread can upgrade read lock
     CompletableFuture<Boolean> ft1 = new CompletableFuture<>();
     CompletableFuture<Boolean> ft2 = new CompletableFuture<>();
-    final String key1 = "testDeadlock1_pesrw";
-    final String key2 = "testDeadlock2_pesrw";
+    // deadlock in t2
+    final String key1 = "testDeadlock1_pesw";
+    final String key2 = "testDeadlock2_pesw";
     CompletableFuture<Boolean> f1 = new CompletableFuture<>();
     CompletableFuture<Boolean> f2 = new CompletableFuture<>();
     try(KvdClient client = client()) {
       assertFalse(client.contains(key1));
       assertFalse(client.contains(key2));
-      client.putString(key1, "1");
-      client.putString(key2, "1");
+      client.putString(key1, "client");
+      client.putString(key2, "client");
       Thread t1 = new Thread(() -> {
         try(KvdTransaction tx = client.beginTransaction()) {
           assertTrue(tx.contains(key1));
-          tx.putString(key1, "2");
+          tx.putString(key1, "1");
           f1.complete(true);
           f2.get();
+          assertEquals("client", tx.getString(key2));
           long startNs = System.nanoTime();
-          assertEquals("2", tx.getString(key2));
+          tx.putString(key2, "1");
           long endNs = System.nanoTime();
           assertTrue(TimeUnit.NANOSECONDS.toMillis(endNs - startNs) > 200);
           tx.commit();
@@ -212,7 +171,7 @@ public class ConcurrencyControlPesRWTest {
           f2.complete(true);
           f1.get();
           Thread.sleep(250);
-          assertThrows(KvdException.class, () -> tx.getString(key1));
+          assertThrows(KvdException.class, () -> tx.putString(key1, "2"));
           tx.commit();
           ft2.complete(true);
         } catch(Throwable t) {
@@ -225,20 +184,20 @@ public class ConcurrencyControlPesRWTest {
       t2.join();
       ft1.get();
       ft2.get();
-      assertEquals("2", client.getString(key1));
-      assertEquals("2", client.getString(key2));
+      assertEquals("1", client.getString(key1));
+      assertEquals("1", client.getString(key2));
     }
   }
 
   @Test
   public void deadlock3() throws Exception {
-    // neither thread can upgrade read lock
+    // t3 can't get write lock
     CompletableFuture<Boolean> ft1 = new CompletableFuture<>();
     CompletableFuture<Boolean> ft2 = new CompletableFuture<>();
     CompletableFuture<Boolean> ft3 = new CompletableFuture<>();
-    final String key1 = "deadlock3_1_pesrw";
-    final String key2 = "deadlock3_2_pesrw";
-    final String key3 = "deadlock3_3_pesrw";
+    final String key1 = "deadlock3_1_pesw";
+    final String key2 = "deadlock3_2_pesw";
+    final String key3 = "deadlock3_3_pesw";
     CompletableFuture<Boolean> f1 = new CompletableFuture<>();
     CompletableFuture<Boolean> f2 = new CompletableFuture<>();
     CompletableFuture<Boolean> f3 = new CompletableFuture<>();
@@ -254,8 +213,6 @@ public class ConcurrencyControlPesRWTest {
           f2.get();
           f3.get();
           long startNs = System.nanoTime();
-          assertTrue(tx.contains(key2));
-          assertEquals("2", tx.getString(key2));
           tx.putString(key2, "1");
           assertEquals("1", tx.getString(key2));
           long endNs = System.nanoTime();
@@ -274,8 +231,6 @@ public class ConcurrencyControlPesRWTest {
           f1.get();
           f3.get();
           long startNs = System.nanoTime();
-          assertTrue(tx.contains(key3));
-          assertEquals("3", tx.getString(key3));
           tx.putString(key3, "2");
           assertEquals("2", tx.getString(key3));
           long endNs = System.nanoTime();
@@ -296,7 +251,7 @@ public class ConcurrencyControlPesRWTest {
           f2.get();
           Thread.sleep(250);
           // this creates the deadlock
-          assertThrows(KvdException.class, () -> tx.getString(key1));
+          assertThrows(KvdException.class, () -> tx.putString(key1, "3"));
           tx.commit();
           ft3.complete(true);
         } catch(Throwable t) {
@@ -315,6 +270,52 @@ public class ConcurrencyControlPesRWTest {
       assertEquals("1", client.getString(key1));
       assertEquals("1", client.getString(key2));
       assertEquals("2", client.getString(key3));
+    }
+  }
+
+  @Test
+  public void readUntilSet() throws Exception {
+    CompletableFuture<Boolean> ft1 = new CompletableFuture<>();
+    CompletableFuture<Boolean> ft2 = new CompletableFuture<>();
+    final String key = "readUntilSet_pesw";
+    try(KvdClient client = client()) {
+      assertFalse(client.contains(key));
+      client.putString(key, "client");;
+      Thread t1 = new Thread(() -> {
+        try(KvdTransaction tx = client.beginTransaction()) {
+          assertTrue(tx.contains(key));
+          tx.putString(key, "1");
+          assertEquals("1", tx.getString(key));
+          Thread.sleep(250);
+          tx.commit();
+          ft1.complete(true);
+        } catch(Throwable t) {
+          ft1.completeExceptionally(new RuntimeException("t1 failed", t));
+        }
+      });
+      Thread t2 = new Thread(() -> {
+        try(KvdTransaction tx = client.beginTransaction()) {
+          assertTrue(tx.contains(key));
+          int counter = 0;
+          while(!tx.getString(key).equals("1")) {
+            counter++;
+            Thread.sleep(5);
+          }
+          assertEquals("1", tx.getString(key));
+          assertTrue(counter > 0);
+          tx.commit();
+          ft2.complete(true);
+        } catch(Throwable t) {
+          ft2.completeExceptionally(new RuntimeException("t2 failed", t));
+        }
+      });
+      t1.start();
+      t2.start();
+      t1.join();
+      t2.join();
+      ft1.get();
+      ft2.get();
+      assertEquals("1", client.getString(key));
     }
   }
 
