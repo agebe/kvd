97c867501df528cf26866bcf124ff54df4264272
Andre Gebers <andre.gebers@gmail.com>
2021-04-06 06:12:37 +1000

    fix: do not dirty git workspace when downloading/extracting protoc. rename master to main

--- kvd-server/build.sh
+++ kvd-server/build.sh
@@ -2,7 +2,7 @@
 set -e
 docker pull gradle:jdk11
 docker pull openjdk:16-buster
-TAG=${1:-master}
+TAG=${1:-main}
 TDIR=`mktemp -d`
 echo $TDIR
 function finish {
@@ -13,16 +13,16 @@ cd $TDIR
 
 git clone https://github.com/agebe/kvd.git
 
-if [ $TAG = 'master' ]; then
-  echo "build from master"
+if [ $TAG = 'main' ]; then
+  echo "build from main"
   DOCKER_TAG='latest'
 else
   echo "build from $TAG"
   ( cd kvd && git checkout tags/$TAG )
   DOCKER_TAG=$TAG
 fi
-
-docker run --rm -ti -u gradle --name "kvd-build" -v "$PWD/kvd":/home/gradle/project -w /home/gradle/project -e "GRADLE_USER_HOME=/home/gradle/project/.gradle" gradle:jdk11 bash -c "wget https://github.com/protocolbuffers/protobuf/releases/download/v3.15.7/protoc-3.15.7-linux-x86_64.zip;unzip -qq protoc-3.15.7-linux-x86_64.zip;export PATH=bin:$PATH;gradle dockerPrepare"
+# make sure downloading of protoc does not dirty the git workspace (all downloaded/extracted files need to be covered by .gitignore) otherwise dirty shows up in version
+docker run --rm -ti -u gradle --name "kvd-build" -v "$PWD/kvd":/home/gradle/project -w /home/gradle/project -e "GRADLE_USER_HOME=/home/gradle/project/.gradle" gradle:jdk11 bash -c "wget -O protoc.zip https://github.com/protocolbuffers/protobuf/releases/download/v3.15.7/protoc-3.15.7-linux-x86_64.zip;unzip -qq protoc bin/protoc;export PATH=/home/gradle/project/bin:$PATH;gradle dockerPrepare"
 ( cd kvd/kvd-server/build/docker && docker build -t kvd:$DOCKER_TAG . )
 
 echo done
