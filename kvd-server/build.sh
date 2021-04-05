a393540ee9038971d06441c88520a64e7648d7a1
Andre Gebers <andre.gebers@gmail.com>
2021-04-06 05:15:59 +1000

    add protoc to docker build

--- kvd-server/build.sh
+++ kvd-server/build.sh
@@ -22,7 +22,7 @@ else
   DOCKER_TAG=$TAG
 fi
 
-docker run --rm -ti -u gradle --name "kvd-build" -v "$PWD/kvd":/home/gradle/project -w /home/gradle/project -e "GRADLE_USER_HOME=/home/gradle/project/.gradle" gradle:jdk11 gradle dockerPrepare
+docker run --rm -ti -u gradle --name "kvd-build" -v "$PWD/kvd":/home/gradle/project -w /home/gradle/project -e "GRADLE_USER_HOME=/home/gradle/project/.gradle" gradle:jdk11 bash -c "wget https://github.com/protocolbuffers/protobuf/releases/download/v3.15.7/protoc-3.15.7-linux-x86_64.zip;unzip -qq protoc-3.15.7-linux-x86_64.zip;export PATH=bin:$PATH;gradle dockerPrepare"
 ( cd kvd/kvd-server/build/docker && docker build -t kvd:$DOCKER_TAG . )
 
 echo done
