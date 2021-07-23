4eada114557c3082e4f747932d04bf307d29c62d
Andre Gebers <andre.gebers@gmail.com>
2021-07-24 06:22:36 +1000

    add a byte array key

--- kvd-server/src/test/java/kvd/test/SimpleTest.java
+++ kvd-server/src/test/java/kvd/test/SimpleTest.java
@@ -6,6 +6,7 @@ public class SimpleTest {
   public static void main(String[] args) throws Exception {
     try(KvdClient client = new KvdClient("localhost")) {
       client.putString("simple", "foo");
+      client.putBytes(new byte[] {0}, "foo".getBytes());
       System.out.println(client.getString("simple"));
     }
   }
