65d06bad5ddaf146eab51072704c109c380bf8f7
Andre Gebers <andre.gebers@gmail.com>
2021-07-25 07:18:02 +1000

    improved test

--- kvd-server/src/test/java/kvd/test/SimpleTest.java
+++ kvd-server/src/test/java/kvd/test/SimpleTest.java
@@ -1,5 +1,9 @@
 package kvd.test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.apache.commons.lang3.StringUtils;
+
 import kvd.client.KvdClient;
 
 public class SimpleTest {
@@ -7,6 +11,10 @@ public class SimpleTest {
     try(KvdClient client = new KvdClient("localhost")) {
       client.putString("simple", "foo");
       client.putBytes(new byte[] {0}, "foo".getBytes());
+      String largeKey = StringUtils.repeat('a', 1000);
+      String val = "large key test";
+      client.putString(largeKey, val);
+      assertEquals(val, client.getString(largeKey));
       System.out.println(client.getString("simple"));
     }
   }
