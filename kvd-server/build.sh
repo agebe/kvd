14af387c21de83ba76a1aec39004c73ee6c4b4d6
Andre Gebers <andre.gebers@gmail.com>
2021-07-27 05:55:34 +1000

    fixed protoc version

--- kvd-server/build.sh
+++ kvd-server/build.sh
@@ -22,7 +22,7 @@ else
   DOCKER_TAG=$TAG
 fi
 # make sure downloading of protoc does not dirty the git workspace (all downloaded/extracted files need to be covered by .gitignore) otherwise dirty shows up in version
-docker run --rm -ti -u gradle --name "kvd-build" -v "$PWD/kvd":/home/gradle/project -w /home/gradle/project -e "GRADLE_USER_HOME=/home/gradle/project/.gradle" gradle:jdk11 bash -c "wget -O protoc.zip https://github.com/protocolbuffers/protobuf/releases/download/v3.15.7/protoc-3.15.7-linux-x86_64.zip;unzip -qq protoc bin/protoc;export PATH=/home/gradle/project/bin:$PATH;gradle dockerPrepare"
+docker run --rm -ti -u gradle --name "kvd-build" -v "$PWD/kvd":/home/gradle/project -w /home/gradle/project -e "GRADLE_USER_HOME=/home/gradle/project/.gradle" gradle:jdk11 bash -c "wget -O protoc.zip https://github.com/protocolbuffers/protobuf/releases/download/v3.17.3/protoc-3.17.3-linux-x86_64.zip;unzip -qq protoc bin/protoc;export PATH=/home/gradle/project/bin:$PATH;gradle dockerPrepare"
 ( cd kvd/kvd-server/build/docker && docker build -t kvd:$DOCKER_TAG . )
 
 echo done
