a8f094f54c8873600faa7e6f212d53512605f429
Andre Gebers <andre.gebers@gmail.com>
2021-12-05 06:23:42 +1000

    added README.md examples

--- kvd-server/src/test/java/kvd/test/SimpleTest.java
+++ kvd-server/src/test/java/kvd/test/SimpleTest.java
@@ -2,12 +2,34 @@ package kvd.test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+
 import org.apache.commons.lang3.StringUtils;
 
 import kvd.client.KvdClient;
 
 public class SimpleTest {
   public static void main(String[] args) throws Exception {
+    // test README.md examples
+    try(KvdClient client = new KvdClient("localhost:3030")) {
+      client.putString("my-key", "my-value");
+      System.out.println(client.getString("my-key"));
+    }
+    try(KvdClient client = new KvdClient("localhost:3030")) {
+      try(DataOutputStream out = new DataOutputStream(client.put("simplestream"))) {
+        out.writeLong(42);
+      }
+      InputStream i = client.get("simplestream");
+      if(i != null) {
+        try(DataInputStream in = new DataInputStream(i)) {
+          System.out.println(in.readLong());
+        }
+      } else {
+        throw new RuntimeException("value missing");
+      }
+    }
     try(KvdClient client = new KvdClient("localhost")) {
 //      client.removeAll();
       if(!client.contains("simple")) {
