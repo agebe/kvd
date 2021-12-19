82168df8b8a05eaddf494b593197263295be1bd3
Andre Gebers <andre.gebers@gmail.com>
2021-12-20 06:15:51 +1000

    removed protoc download from docker build script as no longer required

--- kvd-server/build.sh
+++ kvd-server/build.sh
@@ -20,8 +20,8 @@ else
   ( cd kvd && git checkout tags/$TAG )
   DOCKER_TAG=$TAG
 fi
-# make sure downloading of protoc does not dirty the git workspace (all downloaded/extracted files need to be covered by .gitignore) otherwise dirty shows up in version
-docker run --rm -ti -u gradle --name "kvd-build" -v "$PWD/kvd":/home/gradle/project -w /home/gradle/project -e "GRADLE_USER_HOME=/home/gradle/project/.gradle" gradle:jdk11 bash -c "wget -O protoc.zip https://github.com/protocolbuffers/protobuf/releases/download/v3.19.1/protoc-3.19.1-linux-x86_64.zip;unzip -qq protoc bin/protoc;export PATH=/home/gradle/project/bin:$PATH;gradle dockerPrepare"
+# make sure downloaded tools (e.g. formerly protoc) does not dirty the git workspace. All downloaded/extracted files need to be covered by .gitignore otherwise dirty shows up in version
+docker run --rm -ti -u gradle --name "kvd-build" -v "$PWD/kvd":/home/gradle/project -w /home/gradle/project -e "GRADLE_USER_HOME=/home/gradle/project/.gradle" gradle:jdk11 bash -c "gradle dockerPrepare"
 ( cd kvd/kvd-server/build/docker && docker build --pull -t kvd:$DOCKER_TAG . )
 
 echo done
