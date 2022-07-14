0b990295574cc14e54a445a4b548e0f084afd6a2
Andre Gebers <andre.gebers@gmail.com>
2022-07-15 03:40:17 +1000

    always pull container image on run

--- kvd-server/build.sh
+++ kvd-server/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-docker pull gradle:jdk11
 TAG=${1:-main}
 TDIR=`mktemp -d`
 echo $TDIR
@@ -9,9 +8,7 @@ function finish {
 }
 trap finish EXIT
 cd $TDIR
-
 git clone https://github.com/agebe/kvd.git
-
 if [ $TAG = 'main' ]; then
   echo "build from main"
   DOCKER_TAG='latest'
@@ -21,7 +18,6 @@ else
   DOCKER_TAG=$TAG
 fi
 # make sure downloaded tools (e.g. formerly protoc) does not dirty the git workspace. All downloaded/extracted files need to be covered by .gitignore otherwise dirty shows up in version
-docker run --rm -ti -u gradle --name "kvd-build" -v "$PWD/kvd":/home/gradle/project -w /home/gradle/project -e "GRADLE_USER_HOME=/home/gradle/project/.gradle" gradle:jdk11 bash -c "gradle dockerPrepare"
+docker run --pull always --rm -ti -u gradle --name "kvd-build" -v "$PWD/kvd":/home/gradle/project -w /home/gradle/project -e "GRADLE_USER_HOME=/home/gradle/project/.gradle" gradle:jdk11 bash -c "gradle dockerPrepare"
 ( cd kvd/kvd-server/build/docker && docker build --pull -t kvd:$DOCKER_TAG . )
-
 echo done
